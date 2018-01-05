41ee7a829bf82d46121c36d62afad79a5930af85
zaccharie <zaccharie.meddah@gmail.com>
2018-01-05 15:44:43 +0100

    filtering

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,6 +1,7 @@
 package dev;
 
 import com.github.lalyos.jfiglet.FigletFont;
+import java.util.ResourceBundle;
 
 /**
  * Hello world!
@@ -10,7 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiTitle = FigletFont.convertOneLine("Hello World");
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiTitle = FigletFont.convertOneLine(titre);
         System.out.println( asciiTitle );
+        
+        //affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
